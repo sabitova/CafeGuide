cdbec1c9228eed7f0f7c1c68204e61dd9046209b
Svetlana Sabitova <4exovenus@gmail.com>
2016-03-07 21:13:41 +0300

    Entity Framework added

--- CafeGuide/Context.cs
+++ CafeGuide/Context.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CafeGuide
 {
-    class Context
+    class Context : DbContext
     {
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Cafe> Cafes { get; set; }
