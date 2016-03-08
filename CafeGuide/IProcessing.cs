57c35b97323604dda81cf8dc9106ca0b1ea84028
Svetlana Sabitova <4exovenus@gmail.com>
2016-03-08 03:17:44 +0300

    Data processing

---
+++ CafeGuide/IProcessing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeGuide
+{
+    interface IProcessing
+    {
+        List<Cafe> GetSuitableCafes(int seconds);
+    }
+}
---
+++ CafeGuide/RepoProcessing.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeGuide
+{
+    class RepoProcessing : IProcessing
+    {
+        List<Cafe> Cafes = new List<Cafe>();
+
+        public void AddEntities()
+        {
+            var anticafe = new Type
+            {
+                Id = 1,
+                Name = "anticafe"
+            };
+
+            var bar = new Type
+            {
+                Id = 2,
+                Name = "bar"
+            };
+
+            var address1 = new Address
+            {
+                Id = 1,
+                Text = "Myasnitskaya ul., d. 17, str. 2",
+                Lat = "55.763789",
+                Long = "37.634681"
+            };
+
+            var address2 = new Address
+            {
+                Id = 2,
+                Text = "Pokrovka ul., d. 19",
+                Lat = "55.759794",
+                Long = "37.646223899999995"
+            };
+
+            var eu = new Cuisine
+            {
+                Id = 1,
+                Name = "European"
+            };
+
+            var it = new Cuisine
+            {
+                Id = 2,
+                Name = "Italian"
+            };
+
+            var Cafe1 = new Cafe
+            {
+                Id = 1,
+                Name = "Checkpoint",
+                Type = anticafe,
+                Address = address1,
+                CheckAvg = 300,
+                WiFi = true,
+                Cuisine = new List<Cuisine> { eu }
+            };
+
+            var Cafe2 = new Cafe
+            {
+                Id = 2,
+                Name = "19Bar&Atmosphere",
+                Type = bar,
+                Address = address2,
+                CheckAvg = 1000,
+                WiFi = false,
+                Cuisine = new List<Cuisine> { eu, it }
+            };
+
+            Cafes.Add(Cafe1);
+            Cafes.Add(Cafe2);
+        }
+
+        public List<Cafe> GetSuitableCafes(int seconds)
+        {
+            var SuitableCafes = new List<Cafe>();
+
+            foreach (var c in Cafes)
+            {
+
+            }
+
+            return SuitableCafes;
+        }
+    }
+}
